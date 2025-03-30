5942c5d0ed53631f3b1abb45cabb410c4d36a241
oskarnordin <oskarnordin1@gmail.com>
2025-03-30 20:11:47 +0200

    Update2

--- script.js
+++
@@ -1,7 +0,0 @@
-document.getElementById("hamburger").addEventListener("click", function () {
-    let hamburger = document.getElementById("hamburger");
-    let navMenu = document.getElementById("nav-menu");
-
-    hamburger.classList.toggle("active");
-    navMenu.classList.toggle("show");
-});
