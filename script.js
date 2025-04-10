77fec1c7fb9531e8a6d6028b78117288fa9bbaba
oskarnordin <oskarnordin1@gmail.com>
2025-04-10 12:57:57 +0200

    Changed font. Removed background shading in input field. Resized button elements in navigation menu.

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 document.getElementById("hamburger").addEventListener("click", function () {
-    let hamburger = document.getElementById("hamburger");
-    let navMenu = document.getElementById("nav-menu");
+  let hamburger = document.getElementById("hamburger");
+  let navMenu = document.getElementById("nav-menu");
 
-    hamburger.classList.toggle("active");
-    navMenu.classList.toggle("show");
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("show");
 });
